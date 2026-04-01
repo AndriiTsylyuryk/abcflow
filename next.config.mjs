e73467198783238bb9ebf54a6c53adbd245b6bd1
Andrii Tsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2026-04-01 15:54:58 +0000

    cors upd

--- next.config.mjs
+++ next.config.mjs
@@ -32,6 +32,10 @@ const nextConfig = {
             key: "Permissions-Policy",
             value: "camera=(), microphone=(), geolocation=()",
           },
+          {                                                                           
+key: "Cross-Origin-Opener-Policy",                                        
+  value: "same-origin-allow-popups",                                        
+         },   
         ],
       },
     ];
